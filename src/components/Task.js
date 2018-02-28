896f750c8f539028e4a109f10efdb37b423c79cb
Gin97 <preciadomiguel1997@gmail.com>
2018-02-27 20:25:24 -0600

    Task Component logic to check task and count undone tasks & Scroll added

--- src/components/Task.js
+++ src/components/Task.js
@@ -10,23 +10,31 @@ import {
 export default class Task extends Component{
   constructor(props){
     super(props);
+    console.log(this.props.updateTask);
     //TODO: Define state
   }
 
   render(){
+
+    const {title, completed, id } = this.props.task;
+    const icon = completed ? require('./../images/icon-checked.png') : require('./../images/icon-circle.png')
+
     return(
-      <View key= {this.props.id}style={styles.taskContainer}>
-      <TouchableHighlight>
-        <Image style={styles.taskIcon} source={require('./../images/icon-circle.png')}/>
+      <TouchableHighlight onPress={ ()=> this.props.updateTask(id) } style={styles.container}>
+        <View style={styles.taskContainer}>
+          <Image style={styles.taskIcon} source={icon}/>
+          <Text style={completed ? styles.taskTextCompleted : styles.taskText}>{title}</Text>
+        </View>
       </TouchableHighlight>
-        <Text style={styles.taskText}>{this.props.title}</Text>
-      </View>
     )
   }
 }
 
 
 const styles = StyleSheet.create({
+  container:{
+    height: 60
+  },
   taskContainer: {
     flex: 1,
     backgroundColor: 'white',
@@ -44,5 +52,9 @@ const styles = StyleSheet.create({
   },
   taskText: {
     fontSize: 21
+  },
+  taskTextCompleted: {
+    fontSize: 21,
+    color: '#A4A4A4'
   }
 });
--- src/screens/TaskScreen.js
+++ src/screens/TaskScreen.js
@@ -7,7 +7,8 @@ import {
   View,
   Text,
   Image,
-  StyleSheet
+  StyleSheet,
+  ScrollView
 } from 'react-native';
 
 import TasksHeader from './../components/TasksHeader';
@@ -27,16 +28,50 @@ export default class TaskScreen extends Component {
         { id: 5, title:'Leche', completed: false },
         { id: 6, title:'Basura', completed: false },
         { id: 7, title:'Platos', completed: false },
-        { id: 8, title:'Fiesta', completed: false }
+        { id: 8, title:'Fiesta', completed: false },
+        { id: 9, title:'Leche', completed: false },
+        { id: 10, title:'Basura', completed: false },
+        { id: 11, title:'Platos', completed: false },
+        { id: 12, title:'Fiesta', completed: false },
+        { id: 13, title:'Leche', completed: false },
+        { id: 14, title:'Basura', completed: false },
+        { id: 15, title:'Platos', completed: false },
+        { id: 16, title:'Fiesta', completed: false },
       ]
     }
+  }
+
+  addTask(){
+
+  }
+
+  calculateTaskToBeCompleted(){
+    //Retorna el numero de tareas no completadas
+    let count = 0;
+    this.state.tasks.forEach( task => {
+      if(!task.completed){
+        count++;
+      }
+    } );
+    return count;
+  }
+
+  updateTask(targetId){
+    //copiar estado
+    let currentTask = [...this.state.tasks];
+    //verificar id a cambiar
+    let taskToBeUpdated = currentTask.find( task => task.id === targetId );
+    //checar ver estado y cambiarlo
+    taskToBeUpdated.completed = !taskToBeUpdated.completed;
+    //actualizar estado
+    this.setState( {tasks: currentTask } );
   }
 
   renderTasks(){
     return(
       this.state.tasks.map( (task) => {
           return(
-            <Task key= {task.id} id={task.id} title={task.title} />
+            <Task key= {task.id} task={task} updateTask={this.updateTask.bind(this)}/>
             //<Task id={task.id} tittle={task.tittle} onUpdateTask={this.updateTask}/>
             //updateTask(id) investigar
             //Ejecutar en el componente desde props
@@ -48,10 +83,10 @@ export default class TaskScreen extends Component {
   render(){
     return (
       <View style={ styles.container }>
-        <TasksHeader toBeCompleted={28} />
-        <View style={ styles.tasksContainer }>
+        <TasksHeader toBeCompleted={this.calculateTaskToBeCompleted()} />
+        <ScrollView style={ styles.tasksContainer }>
           { this.renderTasks() }
-        </View>
+        </ScrollView>
       </View>
     )
   }
@@ -62,7 +97,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   tasksContainer: {
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     flex: 1
   }
 });
